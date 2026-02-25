fb8d42230c54b0c23ea38dfb06c9007a8686da16
discraa <68938855+discraa@users.noreply.github.com>
2026-02-25 09:31:37 -0300

    Add core.js

---
+++ core.js
@@ -0,0 +1,61 @@
+// Core script to add-in certain features like commands and auto
+// load scripts that are in discraa's github.
+
+window.dsk = {};
+dsk.loadingScripts = false;
+dsk.commands = new Map(); // Btw i removed `Map` from ml.mim.js
+
+dsk.setCmd = (prefix, callback) => {
+  dsk.commands.set(prefix, callback);
+};
+dsk.deleteCmd = prefix => {
+  dsk.commands.delete(prefix);
+};
+
+dsk.loadScript = src => {
+  return new Promise((res, rej) => {
+    const script = document.createElement('script');
+    script.onload = res;
+    script.onerror = rej;
+    document.body.appendChild(script);
+  });
+};
+
+dsk.loadScripts = async urls => {
+  for (let i = 0; i < urls.length; i++) {
+    const error = await dsk.loadScript(urls[i]);
+    if (error) append(error.stack);
+  }
+};
+
+// Init shit, i think
+dsk.init = () => {
+  dsk.setCmd('/help', () => {
+    append('Commands:');
+    Array.from(dsk.commands.keys()).forEach(prefix => {
+      append(prefix);
+    });
+  });
+
+  dsk.setCmd('/load', async () => {
+    if (dsk.loadingScripts) return;
+    dsk.loadingScripts = true;
+    append('Loading scripts ...');
+
+    fetch('path/to/your/file.txt')
+      .then(response => response.text())
+      .then(data => dsk.loadScripts(JSON.parse(data)))
+      .catch(error => console.error('Error loading file:', error));
+  });
+};
+
+// Ensure load when in-game bc i'm lazy asf
+// (if we are in-game, lots of shit from client loaded already,
+// so we don't have to use our brains)
+dsk.loadInterval = setInterval(() => {
+  if (game_state === GAME_PLAYING) {
+    clearInterval(dsk.loadInterval);
+
+    dsk.init();
+  }
+}, 5);
